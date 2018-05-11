9423fdffa13c678403e6b9894c476f6210b7544e
evertongodoy <everton.mgodoy@gmail.com>
2018-05-11 15:41:52 -0300

    Insercao de menu

--- home/views.py
+++ home/views.py
@@ -12,5 +12,11 @@ def meu_logout(request):
     logout(request)
     return redirect(inicio)
 
+# Everton Godoy - 10/05/2018
+# Se o usuario estiver logado, mostrar botao do Sistema
+#def sistema(request):
+#    pass
+
+
 
 
